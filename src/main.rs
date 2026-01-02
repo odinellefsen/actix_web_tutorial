38f64cbd6a4a9157902aa80e17069b47542a814c
odinellefsen <odin@ellefsen.fo>
2026-01-02 22:01:35 +0000

    Refactor main function in Actix web application: remove AppState struct and streamline server setup with /api prefix for index route.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,5 @@
 use actix_web::{App, HttpResponse, HttpServer, Responder, get, web};
 
-struct AppState {
-    app_name: String,
-}
-
 #[get("/")]
 async fn index() -> impl Responder {
     HttpResponse::Ok().body("Hello World")
@@ -11,14 +7,8 @@ async fn index() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .app_data(web::Data::new(AppState {
-                app_name: String::from("Actix Web"),
-            }))
-            .service(index)
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
+    HttpServer::new(|| App::new().service(web::scope("/api").service(index)))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
