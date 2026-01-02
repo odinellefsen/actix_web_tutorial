269bc21c452a5201a9f8e31e6830429d9351890e
odinellefsen <odin@ellefsen.fo>
2026-01-02 21:46:23 +0000

    Refactor Actix web handlers: replace manual hello with index, remove echo endpoint, and introduce AppState for application data management.

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,22 @@
-use actix_web::{App, HttpResponse, HttpServer, Responder, get, post, web};
+use actix_web::{App, HttpResponse, HttpServer, Responder, get, web};
+
+struct AppState {
+    app_name: String,
+}
 
 #[get("/")]
-async fn hello() -> impl Responder {
+async fn index() -> impl Responder {
     HttpResponse::Ok().body("Hello World")
-}
-
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
-
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
-        App::new().service(
-            web::scope("/api")
-                .service(hello)
-                .service(echo)
-                .route("/hey", web::get().to(manual_hello)),
-        )
+        App::new()
+            .app_data(web::Data::new(AppState {
+                app_name: String::from("Actix Web"),
+            }))
+            .service(index)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
