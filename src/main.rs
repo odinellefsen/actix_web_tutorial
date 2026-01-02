0194a1509553522fe392a565a47b112d43dca2be
odinellefsen <odin@ellefsen.fo>
2026-01-02 23:03:18 +0000

    Add create_todo endpoint to handle POST requests and update requests.http with new todo structure

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,44 @@
-use actix_web::{App, HttpResponse, HttpServer, Responder, get, web};
-use serde::Serialize;
+use actix_web::{App, HttpResponse, HttpServer, Responder, get, post, web};
+use serde::{Deserialize, Serialize};
 use std::sync::{Arc, Mutex};
 
 type Todos = Arc<Mutex<Vec<String>>>;
 
 #[derive(Serialize)]
-struct GetTodosEndpointResponse {
-    message: String,
+struct TodosResponse {
+    message: &'static str,
     todos: Vec<String>,
 }
 
-#[get("/")]
+#[get("")]
 async fn get_todos(todos: web::Data<Todos>) -> impl Responder {
     let todos_guard = todos.lock().unwrap();
 
-    let response = GetTodosEndpointResponse {
-        message: "These are your todos".to_string(),
+    let response = TodosResponse {
+        message: "These are your todos",
         todos: (*todos_guard).clone(),
+    };
+
+    HttpResponse::Ok().json(response)
+}
+
+#[derive(Deserialize)]
+struct CreateTodoRequest {
+    todo: String,
+}
+
+#[post("")]
+async fn create_todo(
+    payload: web::Json<CreateTodoRequest>,
+    todos: web::Data<Todos>,
+) -> impl Responder {
+    let mut todos = todos.lock().unwrap();
+
+    todos.push(payload.todo.clone());
+
+    let response = TodosResponse {
+        message: "Todo Created!",
+        todos: (*todos).clone(),
     };
 
     HttpResponse::Ok().json(response)
@@ -27,9 +49,10 @@ async fn main() -> std::io::Result<()> {
     let todos: Todos = Arc::new(Mutex::new(Vec::new()));
 
     HttpServer::new(move || {
-        App::new()
-            .app_data(web::Data::new(todos.clone()))
-            .service(web::scope("/api").service(web::scope("/todos").service(get_todos)))
+        App::new().app_data(web::Data::new(todos.clone())).service(
+            web::scope("/api")
+                .service(web::scope("/todos").service(get_todos).service(create_todo)),
+        )
     })
     .bind(("127.0.0.1", 8080))?
     .run()
