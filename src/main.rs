3ca58adb22b7f08f5151210d357fb5aef38c5b10
odinellefsen <odin@ellefsen.fo>
2026-01-03 19:51:40 +0000

    bug was creating new shared state instance for each request

--- src/main.rs
+++ src/main.rs
@@ -7,17 +7,17 @@ use todos::structs::Todo;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let shared_todos = web::Data::new(Arc::new(Mutex::new(Vec::<Todo>::new())));
+
     HttpServer::new(move || {
-        App::new()
-            .app_data(web::Data::new(Arc::new(Mutex::new(Vec::<Todo>::new()))))
-            .service(
-                web::scope("/api").service(
-                    web::scope("/todos")
-                        .service(get_todos)
-                        .service(create_todo)
-                        .service(delete_todo),
-                ),
-            )
+        App::new().app_data(shared_todos.clone()).service(
+            web::scope("/api").service(
+                web::scope("/todos")
+                    .service(get_todos)
+                    .service(create_todo)
+                    .service(delete_todo),
+            ),
+        )
     })
     .bind(("127.0.0.1", 8080))?
     .run()
